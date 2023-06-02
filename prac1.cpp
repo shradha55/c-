3320b9bb5d285b37d4cb56016d1aa64efb651754
Shradha suman pal <114575485+shradha55@users.noreply.github.com>
2023-06-02 22:18:07 +0530

    Add files via upload

---
+++ prac1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1,num2;
+    cout<<"Enter the num1:\n";
+    cin>>num1;
+    cout<<"Enter the num2:\n";
+    cin>>num2;
+    cout<<"The sum of the two numbers is:"<<num1+num2;
+    return 0;
+}
---
+++ prac2.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=2,b=5;
+    cout<<"Tne operators in c++:"<<endl;
+    cout<<"The following are the operations:"<<endl;
+    cout<<"the value of a + b is:"<<a+b<<endl;
+    cout<<"the value of a - b is:"<<a-b<<endl;
+    cout<<"the value of a * b is:"<<a*b<<endl;
+    cout<<"the value of a / b is:"<<a/b<<endl;
+    cout<<"the value of a % b is:"<<a%b<<endl;
+    cout<<"the value of a ++ is:"<<a++<<endl;
+    cout<<"the value of a -- is:"<<a--<<endl;
+    cout<<"the value of ++ a is:"<<++a<<endl;
+    cout<<"the value of -- a is:"<<--a<<endl;
+    cout<<"the value of b ++ is:"<<b++<<endl;
+    cout<<"the value of b -- is:"<<b--<<endl;
+    cout<<"the value of ++ b is:"<<++b<<endl;
+    cout<<"the value of -- b is:"<<--b<<endl;
+    return 0;
+}
